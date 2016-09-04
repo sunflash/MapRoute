9699cba570138608c8a6daee2fb067a16eb0900c
Min Wu <min@cellpointmobile.com>
2016-09-04 17:04:50 +0200

    Make overlays work.

--- MapRoute/DataSource/DataSource.swift
+++ MapRoute/DataSource/DataSource.swift
@@ -7,8 +7,73 @@
 //
 
 import Foundation
+import MapKit
+
+struct FareZone {
+    
+    let name: String
+    let zoneNumber: String
+    let neighbourZones: Array<String>
+    let centerCoordinate: CLLocationCoordinate2D
+    let polygon: MKPolygon
+}
 
 class DataSource {
+    
+    static let sharedDataSource = DataSource()
+    
+    func zoneData(completion: @escaping ([String:FareZone],[MKPolygon],[MKPointAnnotation])->Void)   {
+        
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated).async {
+        
+            let fileURL = Bundle.main.url(forResource: "ZealandZones", withExtension: "json")
+            guard let url = fileURL else {return}
+            let jsonData = try? Data(contentsOf: url)
+            guard let data = jsonData else {return}
+            let zealand = JSON(data: data)
+            
+            let polygon: (JSON) -> MKPolygon? =  { coordinates in
+                
+                var points = [CLLocationCoordinate2D]()
+                for (_, coordinateO) in coordinates {
+                    guard let coordinate = coordinateO.array, let latitude = coordinate[1].double, let longitude = coordinate[0].double else {continue}
+                    points += [CLLocationCoordinate2D(latitude: latitude, longitude: longitude)]
+                }
+                guard points.count != 0 else {return nil}
+                return MKPolygon(coordinates: &points, count: points.count)
+            }
+            
+            var zoneData = [String:FareZone]()
+            var zonePolygons = [MKPolygon]()
+            var zoneAnnotations = [MKPointAnnotation]()
+            
+            for (_, zoneInfo) in zealand["features"] {
+                
+                guard let geometryType = zoneInfo["geometry","type"].string, geometryType == "Polygon", let polygon = polygon(zoneInfo["geometry","coordinates"][0]) else {continue}
+                
+                let nameO = zoneInfo["properties","Name"].string
+                let zoneNumberO = zoneInfo["properties","Shortname"].string
+                let neighbourZonesO = zoneInfo["properties","NeighbourZones"].string?.characters.split(separator: ",").map(String.init)
+                let centerCoordinateO = zoneInfo["properties","PolygonCentroid"].string?.characters.split(separator: ",").map(String.init)
+                guard let name = nameO, let zoneNumber = zoneNumberO, let neighbourZones = neighbourZonesO else {continue}
+                guard let centerCoordinate = centerCoordinateO, let centerLat = Double(centerCoordinate[1]), let centerLon = Double(centerCoordinate[0]) else {continue}
+                let center = CLLocationCoordinate2D(latitude: centerLat, longitude: centerLon)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = center
+                annotation.title = zoneNumber
+                zoneAnnotations += [annotation]
+                
+                polygon.title = zoneNumber
+                zonePolygons += [polygon]
+                
+                let zone = FareZone(name: name, zoneNumber: zoneNumber, neighbourZones: neighbourZones, centerCoordinate: center, polygon: polygon)
+                zoneData[zoneNumber] = zone
+            }
+            
+            completion(zoneData,zonePolygons,zoneAnnotations)
+       }
+    }
     
     class func highLightZones() -> Array<String> {
     
@@ -17,5 +82,4 @@ class DataSource {
     
         return highlightZonesArray
     }
-    
 }
--- MapRoute/ViewController/MapViewController.swift
+++ MapRoute/ViewController/MapViewController.swift
@@ -12,16 +12,18 @@ import MapKit
 class MapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak private var mapView : MKMapView!
-
+    
+    private var zoneInfo = [String:FareZone]()
+    private var polygons = [MKPolygon]()
+    private var annotations = [MKPointAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.addOverlays()
         self.mapView.showsPointsOfInterest = false
         self.mapView.delegate = self
-
-        
-        print("\(DataSource.highLightZones())")
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,7 +31,49 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    private func addOverlays() {
+        
+        DataSource.sharedDataSource.zoneData { zoneInfo, polygons, annotations in
+            
+            DispatchQueue.main.async {
+                
+                self.zoneInfo = zoneInfo
+                self.polygons = polygons
+                self.annotations = annotations
+                
+                guard self.polygons.count != 0, self.annotations.count != 0 else {return}
+                self.mapView.addOverlays(self.polygons, level: MKOverlayLevel.aboveLabels)
+                self.mapView.showAnnotations(self.annotations, animated: false)
+            }
+        }
+    }
     
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        if overlay is MKPolygon {
+            
+            let render = MKPolygonRenderer(overlay: overlay)
+            render.strokeColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+            render.lineWidth = 3;
+            render.fillColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            return render
+        }
+        return  MKOverlayRenderer(overlay: overlay)
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+ 
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let identifier = "place"
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) ?? MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        annotationView.annotation = annotation
+        annotationView.canShowCallout = true
+        annotationView.image = #imageLiteral(resourceName: "first")
+
+        return annotationView
+    }
 }
 
