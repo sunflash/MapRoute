deb510c49d6d5d151b4e465dd6ed7aa266328d79
Min Wu <min@cellpointmobile.com>
2016-09-06 16:17:43 +0200

    Update highlight zones number.

--- MapRoute/DataSource/DataSource.swift
+++ MapRoute/DataSource/DataSource.swift
@@ -75,10 +75,10 @@ class DataSource {
        }
     }
     
-    class func highLightZones() -> Array<String> {
+    class func highLightZones() -> Set<String> {
     
-        let highlightZonesArray = ["2","32","43","54","66","76","8","96","97","28","27","140","141",
-                                    "269","262","263","275","277","26","22","20","99","89","77","67","33","44","55","260"];
+        let highlightZonesArray = Set(["1","2","32","43","54","66","76","8","96","97","28","27","140","141",
+                                    "269","262","263","275","277","26","22","20","99","89","77","67","33","44","55","260"])
     
         return highlightZonesArray
     }
